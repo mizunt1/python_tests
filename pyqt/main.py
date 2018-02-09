4024bb7c96f5792a07c50b34edf91b76580d5b80
Mizu Nishikawa-Toomey <mizu.toomey@nanoporetech.com>
2018-02-09 15:42:45 +0000

    works with progress bar

--- pyqt/main.py
+++ pyqt/main.py
@@ -12,6 +12,7 @@ class Main(QMainWindow):
         super(Main, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.progressBar.setValue(0)
         self.ui.pushButton.clicked.connect(self.start_generation)
         self.show()
         
@@ -20,6 +21,7 @@ class Main(QMainWindow):
         self.thread_class = StartGenerationThread(number_of_numbers)
         # start thread
         self.thread_class.number_updated_signal.connect(self.add_to_list)
+        self.thread_class.new_number_signal.connect(self.progress)
         # connect thread signal to function
         self.thread_class.start()
         # start thread
@@ -29,23 +31,28 @@ class Main(QMainWindow):
     def add_to_list(self, number):
         self.ui.output_view.addItem(str(number))
 
+    @pyqtSlot(int)
+    def progress(self, percentage):
+        self.ui.progressBar.setValue(percentage*10)
 
+        
 class StartGenerationThread(QThread):
     number_updated_signal = pyqtSignal(int)
-
+    new_number_signal = pyqtSignal(int)
+    
     def __init__(self, number_of_numbers):
         QThread.__init__(self)
         self.number_of_numbers = number_of_numbers
         
     def start_generation(self):
         time.sleep(2)
-        return np.random.randint(1, self.number_of_numbers)
+        return np.random.randint(1, 10)
         
     def run(self):
         for i in range(self.number_of_numbers):
             top_number = self.start_generation()
             self.number_updated_signal.emit(top_number)
-            print(top_number)
+            self.new_number_signal.emit((i/self.number_of_numbers)*10)
 
             
 def main():
